78b0073c6fc12b8df703f0a3374a17ffd836c1d7
FlorianMMI <florian.bounissou@etu.unlim.fr>
2024-12-09 10:29:24 +0100

    mise à jours de la base de donnée

--- bdd/SAE303-MARKET.sql
+++ bdd/SAE303-MARKET.sql
@@ -1,27 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost:8889
--- Généré le : ven. 06 déc. 2024 à 16:35
--- Version du serveur :  5.7.34
--- Version de PHP : 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `SAE303-MARKET`
---
-CREATE DATABASE IF NOT EXISTS `SAE303-MARKET` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `SAE303-MARKET`;
 
 -- --------------------------------------------------------
 
